335d0de809833e95eec686408028d542187b5456
sigetg <sigetyg@bc.edu>
2023-01-19 21:12:55 -0500

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by George Sigety on 1/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by George Sigety on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
