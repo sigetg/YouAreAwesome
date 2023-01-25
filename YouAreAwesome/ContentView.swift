2eb9ae0f48038f7fc67c08b90d38968ff7ae9016
sigetg <sigetyg@bc.edu>
2023-01-24 21:41:03 -0500

    Completed Video 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,35 +8,42 @@
 import SwiftUI
  
 struct ContentView: View {
-    @State private var messageString = "When the genius bar needs help, they call you!"
+    @State private var messageString = "When the Genius Bar needs help, they call you!"
     
     var body: some View {
+        
+            VStack {
 
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 1)
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Button("Awesome") {
+                        // this is the action
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
                 .padding()
-            
-            HStack {
-                Button("Awesome") {
-                    // this is the action
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
-                Button("Great") {
-                    messageString = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
             }
-            .border(.orange, width: 1)
-        }
     }
 }
 
