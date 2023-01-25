9757b71f6c4273abd7434cb1148261b749c39755
sigetg <sigetyg@bc.edu>
2023-01-24 22:08:46 -0500

    Completed Video 1.7

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,44 +6,45 @@
 //
 
 import SwiftUI
- 
+
 struct ContentView: View {
     @State private var messageString = "When the Genius Bar needs help, they call you!"
     
     var body: some View {
         
-            VStack {
-
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                
-                Spacer()
-                
-                HStack {
-                    Button("Awesome") {
-                        // this is the action
-                        messageString = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
+        
+        VStack {
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
                 .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    // this is the action
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
+            .padding()
+        }
     }
 }
 
