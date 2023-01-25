fc5b780a48bec09866458efac9e20b2765ecac7e
sigetg <sigetyg@bc.edu>
2023-01-24 20:39:11 -0500

    Completed Video 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,17 +6,22 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "When the genius bar needs help, they call you!"
     
     var body: some View {
-        
+
         VStack {
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
                 .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 1)
                 .padding()
             
             HStack {
@@ -30,6 +35,7 @@ struct ContentView: View {
                 }
                 .buttonStyle(.borderedProminent)
             }
+            .border(.orange, width: 1)
         }
     }
 }
